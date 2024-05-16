75369cab43e5c2d25e7365e84351473132c36ac0
Damdoshi <damdoshi@hotmail.com>
2024-05-16 22:10:54 +0200

    Début de reflexion sur le format Dabsic à construire.

---
+++ include/tadventure.h
@@ -0,0 +1,24 @@
+/*
+** Jason Brillante "Damdoshi"
+** Hanged Bunny Studio 2014-2024
+** Pentacle Technologie 2008-2024
+**
+** TAdventure
+*/
+
+#ifndef			__TADVENTURE_H__
+# define		__TADVENTURE_H__
+# ifndef		NOSCREEN
+#  include		<lapin.h>
+# endif
+
+typedef struct		s_program
+{
+# ifndef		NOSCREEN
+  t_bunny_window	*window;
+#endif
+
+  
+}			t_program;
+
+#endif	/*		__TADVENTURE_H__	*/
